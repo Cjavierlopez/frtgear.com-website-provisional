4ce7a472a209b4e84efa119e5b27b56a1be4d2ab
Javier López <117862224+Cjavierlopez@users.noreply.github.com>
2024-03-25 23:34:30 +0100

    vercel analitycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
-import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
+import react from '@astrojs/react';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
-  integrations: [react(), tailwind()]
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
+  integrations: [react(), tailwind()],
 });
